d027839adbdef1a7d4baeba53c0c6152b5d7e034
merl-mewiz <borodayal@gmail.com>
2019-04-25 16:40:46 +0400

    in process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'registration'}
+  devise_for :users, path: '', path_names: { sign_in: 'login', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'register', sign_out: 'signout' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
